2fd90c804a55559ce394886a1533662074d785dc
EduardoBran <eduardo.ads1814@gmail.com>
2023-12-21 18:10:09 -0300

    limpeza e transformacao dos dados - parte 1

--- 02-Carga_e_Transformacao.R
+++ 02-Carga_e_Transformacao.R
@@ -59,3 +59,72 @@ if(Azure){
 #?scale
 str(bikes)
 View(bikes)
+
+# Criar uma nova variável para indicar dia da semana (workday)
+bikes$isWorking <- ifelse(bikes$workingday & !bikes$holiday, 1, 0)  
+
+# Adicionar uma coluna com a quantidade de meses, o que vai ajudar a criar o modelo
+bikes <- month.count(bikes)
+
+# Criar um fator ordenado para o dia da semana, comecando por segunda-feira
+# Neste fator eh convertido para ordenado numérico para ser compativel com os tipos de dados do Azure ML
+bikes$dayWeek <- as.factor(weekdays(bikes$dteday))
+
+str(bikes)
+View(bikes)
+
+
+
+############ ATENÇÃO ############
+
+# ==> Analise o dataframe bikes. 
+# Se os nomes dos dias da semana estiverem em português na coluna bikes$dayWeek, 
+# execute o Bloco1 abaixo, caso contrátio, execute o Bloco2 com os nomes em inglês. 
+# Execute um bloco ou o outro.
+str(bikes$dayWeek)
+
+# Bloco1
+# Se o seu sistema operacional estiver em portugês, execute o comando abaixo.
+bikes$dayWeek <- as.numeric(ordered(bikes$dayWeek, 
+                                    levels = c("segunda-feira", 
+                                               "terça-feira", 
+                                               "quarta-feira", 
+                                               "quinta-feira", 
+                                               "sexta-feira", 
+                                               "sábado", 
+                                               "domingo")))
+
+# Bloco2
+# Se o seu sistema operacional estiver em inglês, execute o comando abaixo.
+bikes$dayWeek <- as.numeric(ordered(bikes$dayWeek, 
+                                    levels = c("Monday", 
+                                               "Tuesday", 
+                                               "Wednesday", 
+                                               "Thursday", 
+                                               "Friday", 
+                                               "Saturday", 
+                                               "Sunday")))
+
+# Agora os dias da semana devem estar como valores numéricos
+# Se estiverem como valores NA, volte e verifique se você seguiu as instruções acima.
+str(bikes$dayWeek)
+str(bikes)
+
+# Adiciona uma variável com valores únicos para o horário do dia em dias de semana e dias de fim de semana
+# Com isso diferenciamos as horas dos dias de semana, das horas em dias de fim de semana
+bikes$workTime <- ifelse(bikes$isWorking, bikes$hr, bikes$hr + 24) 
+
+# Transforma os valores de hora na madrugada, quando a demanda por bibicletas é praticamente nula 
+bikes$xformHr <- ifelse(bikes$hr > 4, bikes$hr - 5, bikes$hr + 19)
+
+# Adiciona uma variável com valores únicos para o horário do dia para dias de semana e dias de fim de semana
+# Considerando horas da madrugada
+bikes$xformWorkHr <- ifelse(bikes$isWorking, bikes$xformHr, bikes$xformHr + 24) 
+
+# str(bikes)
+# View(bikes)
+# O trabalho que fizemos até aqui também é chamado de Feature Engineering ou 
+# Engenharia de Atributos
+
+# Gera saída no Azure ML
+if(Azure) maml.mapOutputPort('bikes')
